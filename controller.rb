36c246a9818b4dc6f2330b38517ef02238068b92
WinnyDrancourt <winny.morel@orange.fr>
2024-02-05 07:16:00 +0100

    First commit

---
+++ controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < Sinatra::Base
+  get '/' do
+    "<html><head><title>The Gossip Project</title></head><body><h1>Mon supersite de gossip !</h1></body></html>"
+  end
+end
